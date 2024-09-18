aa963e3dcbad62806f9c7e75303624bb619fa71a
shumani Simeli <shumanisimeli@gmail.com>
2024-09-18 19:20:21 +0200

    initaialized user registration

--- quiz_app/forms.py
+++ quiz_app/forms.py
@@ -1,7 +1,7 @@
 """Module consist of forms for user authentication"""
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class LoginForm(FlaskForm):
     """Implements user login with all the required fields"""
@@ -11,3 +11,15 @@ class LoginForm(FlaskForm):
                              validators=[DataRequired(), Length(min=8)])
     login = SubmitField('Login')
     remember_me = BooleanField('Remenber me')
+
+class RegistrationForm(FlaskForm):
+    """handles user registration form"""
+    username = StringField('Username', 
+                           validators=[DataRequired(), Length(min=5, max=15)])
+    email = StringField('Email', 
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', 
+                             validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField('Confirm password', 
+                                     validators=[EqualTo('password')])
+    submit = SubmitField('Sign Up')
--- quiz_app/routes.py
+++ quiz_app/routes.py
@@ -1,9 +1,9 @@
 """Module consist of routes/views for application"""
 from flask import Blueprint, render_template, flash, request, redirect, url_for
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user
 from .models import User
-from . import bcrypt
+from . import bcrypt, db
 
 
 views = Blueprint('views', __name__)
@@ -18,16 +18,42 @@ def login():
     """Handles the login url('/login') and renders the login page"""
     form = LoginForm()
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('views.index'))
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember_me.data)
             next_page = request.args.get('next')
             flash(f'Welcome back {user.username}', 'success')
-            return redirect(next_page) if next_page else redirect(url_for('/'))
+            return redirect(next_page) if next_page else redirect(url_for('views.index'))
         else:
-            print('here')
             flash('Login Unsuccessful. Please check email and/or password', 'error')
     return render_template('login.html', form=form)
 
+@views.route('/signup', methods=['GET', 'POST'])
+def sign():
+    """Renders signup templates"""
+    if current_user.is_authenticated:
+        return redirect(url_for('views.index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        email = User.query.filter_by(email=form.email.data).first()
+        if user is None and email is None:
+            hashed_pass = bcrypt.generate_password_hash(form.password.data)
+            user = User(username=form.username.data, email=form.email.data, 
+                password=hashed_pass)
+            db.session.add(user)
+            db.session.commit()
+            flash(f'Account successfully created for {form.username.data}', 'success')
+            return redirect(url_for('views.login'))
+        else:
+            if user and email:
+                flash(f'{form.username.data} and {form.email.data} already exist', 'error')
+            elif user:
+                flash(f'{form.username.data} exists', 'error')
+            elif email:
+                flash(f'{form.email.data} exists', 'error')
+    elif request.method == 'POST':
+        flash('Error occurred creating account', 'error')
+    return render_template('signup.html', form=form)
