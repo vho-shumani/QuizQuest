67c6630772833f1721bce8c111111eb887bfc26a
shumani <shumanisimeli@gmail.com>
2024-10-27 17:30:18 +0200

    initialized database

--- quiz_app/__init__.py
+++ quiz_app/__init__.py
@@ -1,6 +1,10 @@
 """Module contain create_app function which created flask application"""
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import os
+
+
+db = SQLAlchemy()
 
 
 def create_app():
@@ -9,8 +13,15 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or \
         '648168738ED817547FC7A2D2CE2C9'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or \
+        'sqlite:///users.db'
+    
+    db.init_app(app)
     
     from .route import views
     app.register_blueprint(views, url_prefix="/")
+
+    with app.app_context():
+        db.create_all()
 
     return app
